1becb02595cdbf87cd17ee68c9f39f69433e57cf
omri eitan <omrieitan@icloud.com>
2019-01-14 12:00:03 +0200

    refactor -> adding try& catch to model

--- src/com/fat2fit/model/HibernateCategoryDAO.java
+++ src/com/fat2fit/model/HibernateCategoryDAO.java
@@ -26,7 +26,7 @@ public class HibernateCategoryDAO implements ICategoryDAO {
         //the basic way to save new object by session
         if (getCategory(category.getId()) == null) {
             Session session = factoryInstance.getFactory().openSession();
-            Transaction tx=null;
+            Transaction tx = null;
             try {
                 tx = session.beginTransaction();
                 session.save(category);
@@ -47,13 +47,12 @@ public class HibernateCategoryDAO implements ICategoryDAO {
         Category category = getCategory(id);
         if (category != null) {
             Session session = factoryInstance.getFactory().openSession();
-            Transaction tx=null;
+            Transaction tx = null;
             try {
                 tx = session.beginTransaction();
-            session.delete(category);
+                session.delete(category);
                 tx.commit();
-            }
-            catch (HibernateException e) {
+            } catch (HibernateException e) {
                 if (tx != null) tx.rollback();
                 throw new DBException("delete category error", e);
             } finally {
@@ -66,23 +65,41 @@ public class HibernateCategoryDAO implements ICategoryDAO {
     public Category getCategory(int id) throws DBException {
         //the basic way to get  one object by session
         Session session = factoryInstance.getFactory().openSession();
-        session.beginTransaction();
-        Category category = (Category) session.get(Category.class, id);
-        session.close();
+        Transaction tx = null;
+        Category category = null;
+        try {
+            tx = session.beginTransaction();
+            category = (Category) session.get(Category.class, id);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            throw new DBException("get category error", e);
+        } finally {
+            session.close();
+            return category;
+        }
 
-        return category;
+
     }
 
     @Override
     public Category[] getCategories() throws DBException {
         Session session = factoryInstance.getFactory().openSession();
-        session.beginTransaction();
-        List categories = session.createQuery("FROM com.fat2fit.model.Category").list();// hql
-        session.close();
-        //converting list to array
-        Category[] returnArr = new Category[categories.size()];
-        returnArr = (Category[]) categories.toArray(returnArr);
-        return returnArr;
+        Transaction tx = null;
+        List categories = null;
+        try {
+            tx = session.beginTransaction();
+            categories = session.createQuery("FROM com.fat2fit.model.Category").list();// hql
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            throw new DBException("get all category error", e);
+        } finally {
+            session.close();
+            Category[] returnArr = new Category[categories.size()];
+            returnArr = (Category[]) categories.toArray(returnArr);
+            return returnArr;
+        }
     }
 
     @Override
@@ -91,12 +108,23 @@ public class HibernateCategoryDAO implements ICategoryDAO {
         this function converts the name category to category object.
         */
         Session session = factoryInstance.getFactory().openSession();
-        session.beginTransaction();
-        List category = session.createQuery("FROM com.fat2fit.model.Category category WHERE Name=:parm ").setParameter("parm", name).list();// hql
-        session.close();
-        if (category.size() == 0) //there is no category with this name
-            return null;
-        else
-            return (Category) category.get(0);
+        Transaction tx = null;
+        List category = null;
+        try {
+            tx = session.beginTransaction();
+            category = session.createQuery("FROM com.fat2fit.model.Category category WHERE Name=:parm ").setParameter("parm", name).list();// hql
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            throw new DBException("get category by name error", e);
+        } finally {
+            session.close();
+            if (category.size() == 0) //there is no category with this name
+                return null;
+            else
+                return (Category) category.get(0);
+        }
     }
+
+
 }
